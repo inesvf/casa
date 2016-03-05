94c4edf5debd81522808fde83e783856a20d34ed
Ines Vera <inesvf.ciclo@gmail.com>
2016-03-05 20:30:22 +0100

    a ver si sale mezclado con eres mejor

--- casa/src/casa/holahola.java
+++ casa/src/casa/holahola.java
@@ -2,7 +2,11 @@ package casa;
 
 public class holahola {
 	 public static void main(String[] args) {
-		    System.out.println("Hola pepita patata frita");
+
 		    System.out.println("Eres la mejor");
+
+		  
+		    System.out.println("A ver si ahora sale");
+
 		  }
 }
