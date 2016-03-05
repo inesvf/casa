1113fe6f4af8f97cb1a307b088549b5884be0c66
Ines Vera <inesvf.ciclo@gmail.com>
2016-03-05 20:11:15 +0100

    eoeoe

--- casa/src/casa/holahola.java
+++ casa/src/casa/holahola.java
@@ -2,6 +2,7 @@ package casa;
 
 public class holahola {
 	 public static void main(String[] args) {
-		    System.out.println("Hola patata verde");
+		    System.out.println("Eres la mejor");
+
 		  }
 }
