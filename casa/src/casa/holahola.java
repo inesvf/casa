c9ef69a2a1c9e435c5ed97613701c378d197670e
inesvf <inesvf.ciclo@gmail.com>
2016-03-05 20:26:01 +0100

    Update holahola.java

--- casa/src/casa/holahola.java
+++ casa/src/casa/holahola.java
@@ -2,7 +2,7 @@ package casa;
 
 public class holahola {
 	 public static void main(String[] args) {
-		    System.out.println("Hola patata frita");
-		    System.out.println("Eres la mejor");
+		  
+		    System.out.println("A ver si ahora sale");
 		  }
 }
